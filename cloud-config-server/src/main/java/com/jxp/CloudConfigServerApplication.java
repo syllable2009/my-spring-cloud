a44f8b9891e980fc11ebf059c556c4f1e09621a6
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2018-07-30 17:16:33 +0800

    update:2018-7-30

--- cloud-config-server/src/main/java/com/jxp/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/jxp/CloudConfigServerApplication.java
@@ -1,14 +1,33 @@
 package com.jxp;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableConfigServer
 @SpringBootApplication
 public class CloudConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigServerApplication.class, args);
+    }
+
+    @Value("hello")
+    String hello;
+    @Value("app.id")
+    String appId;
+
+    public ResponseEntity getParam() {
+        Map map = new HashMap<>();
+        map.put("hello", hello);
+        map.put("app,id", appId);
+        return ResponseEntity.ok(map);
     }
 }
