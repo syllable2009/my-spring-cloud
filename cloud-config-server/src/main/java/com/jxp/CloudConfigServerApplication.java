1b1f8acef54aa3e410bd0205721621689147bf36
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2019-03-02 17:39:39 +0800

    update:modify

--- cloud-config-server/src/main/java/com/jxp/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/jxp/CloudConfigServerApplication.java
@@ -4,30 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableConfigServer
 @SpringBootApplication
-public class CloudConfigServerApplication {
+public class CloudConfigServerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudConfigServerApplication.class, args);
     }
 
-    @Value("hello")
-    String hello;
+    @Value("app.domain")
+    String domain;
     @Value("app.id")
     String appId;
 
+    @GetMapping("/hello")
     public ResponseEntity getParam() {
         Map map = new HashMap<>();
-        map.put("hello", hello);
-        map.put("app,id", appId);
-        return ResponseEntity.ok(map);
+        map.put("id", appId);
+        map.put("domain", domain);
+        return ResponseEntity.ok(map.toString());
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(">>>>>>>>>>>>>>>CloudConfigServerApplication服务启动执行 <<<<<<<<<<<<<");
     }
 }
