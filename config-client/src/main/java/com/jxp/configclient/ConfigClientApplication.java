804c2f521977ebdbd913c3c914825c8a90a5175b
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2019-03-08 17:57:04 +0800

    update:config

--- config-client/src/main/java/com/jxp/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/jxp/configclient/ConfigClientApplication.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
