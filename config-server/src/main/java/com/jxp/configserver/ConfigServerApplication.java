e540448246c264b6dd6ecdf643bbd78653960b8c
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2019-06-21 14:38:54 +0800

    update:modify code

--- config-server/src/main/java/com/jxp/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/jxp/configserver/ConfigServerApplication.java
@@ -2,11 +2,10 @@ package com.jxp.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableDiscoveryClient
-//@EnableEurekaClient
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
